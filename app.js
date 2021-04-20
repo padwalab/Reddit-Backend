4c9939f1335f946ee25624b3be47a32db28bea82
DeepakVK <deepak.vellorekarunamoorthy@sjsu.edu>
2021-04-20 10:48:50 -0700

    resolved conflicts

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ import userRoute from './server/routes/users.js';
 import registerRoute from './server/routes/users.js';
 import inviteRoute from './server/routes/invites.js';
 import messageRoute from './server/routes/messages.js';
-<<<<<<< HEAD
-=======
-import imageRoute from './server/routes/image.js';
->>>>>>> master
 import db from './server/models/index.js';
 import passport from 'passport';
 import ps from './server/config/passport.js';
