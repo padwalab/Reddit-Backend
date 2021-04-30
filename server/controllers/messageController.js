e0b3c59913f9f0f8f9a562a3716c4e654099eff9
DeepakVK <deepak.vellorekarunamoorthy@sjsu.edu>
2021-04-29 20:35:24 -0700

    Add API to messages

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,11 +1,11 @@
-import Message from "../models/Message.js";
+import Message from '../models/Message.js';
 
 export let messageController = {};
 
 // @route GET api/messages/getMessages
 // @desc conversation between two users
-// @access Public
-messageController.getMessages = async (req, res) => {
+// @access Private
+messageController.getConversation = async (req, res) => {
   const { toUserId, fromUserId } = req.body;
   let messages = await Message.find({ toUserId, fromUserId });
   if (messages) res.status(200).send(messages);
@@ -13,10 +13,29 @@ messageController.getMessages = async (req, res) => {
 
 // @route POST api/messages/sendMessage
 // @desc send a message to other user
-// @access Public
+// @access Private
 messageController.sendMessage = async (req, res) => {
   const { toUserId, fromUserId, text } = req.body;
   let message = new Message({ toUserId, fromUserId, text });
   message.save();
   res.json(message);
+};
+
+// @route POST api/messages/
+// @desc get all conversation specific to a user
+// @access Private
+messageController.userMessages = async (req, res) => {
+  try{
+  const { userId } = req.body;
+  await Message.find()
+    .or([{ toUserId: userId }, { fromUserId: userId }])
+    .populate('toUserId', 'firstName')
+    .populate('fromUserId', 'firstName')
+    .then((messages) => {
+      res.send(messages);
+    });
+  }catch (error) {
+    console.log(error);
+    res.status(500).send('Server error');
+  }
 };
--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,9 +1,10 @@
-import express from "express";
-import { messageController } from "../controllers/messageController.js";
-import { auth } from "../config/auth.js";
+import express from 'express';
+import { messageController } from '../controllers/messageController.js';
+import { auth } from '../config/auth.js';
 
 const router = express.Router();
 export default router;
 
-router.get("/getMessages", auth, messageController.getMessages);
-router.post("/sendMessage", auth, messageController.sendMessage);
+router.get('/getMessages', auth, messageController.getConversation);
+router.post('/', auth, messageController.sendMessage);
+router.get('/', auth, messageController.userMessages);
