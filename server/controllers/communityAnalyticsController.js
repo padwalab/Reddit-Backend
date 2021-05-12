239825b88a6c3e3f04784b279e3598984a51d4c3
Abhijeet Padwal <38448205+padwalab@users.noreply.github.com>
2021-05-11 21:48:42 -0700

    community analytics and search kafka interration

--- server/controllers/communityAnalyticsController.js
+++ server/controllers/communityAnalyticsController.js
@@ -1,70 +1,91 @@
 import { sqlDB } from "../config/queries.js";
 import Community from "../models/Community.js";
 
+import { communityAnalyticsReqProducer } from "../kafka/producers/communityAnalyticsReqProducer.js";
+import { responses } from "../kafka/kafka.js";
+import { communityAnalyticsResConsumer } from "../kafka/consumers/communityAnalyticsResConsumer.js";
+
+communityAnalyticsReqProducer.connect();
+
 export let communityAnalyticsController = {};
 
 // @route GET api/community-analytics/
 // @desc analytics
 // @access Private
 communityAnalyticsController.analytics = async (req, res) => {
-  const communities = await Community.find({ creatorID: req.user.id }).limit(
-    10
-  );
-  if (communities.length > 0) {
-    let communityIds = [];
-    const communityInfo = communities.map((community) => {
-      return {
-        communityName: community.communityName,
-        userCount: community.subscribers.length,
-      };
-    });
-    const postInfo = communities.map((community) => {
-      return {
-        communityName: community.communityName,
-        postCount: community.posts.length,
-      };
-    });
+  const requestId = Math.random().toString(36).substr(2);
+  responses[requestId] = res;
+  console.log(requestId);
+  communityAnalyticsReqProducer.send({
+    topic: "analytics_request",
+    messages: [
+      {
+        value: JSON.stringify({
+          id: requestId,
+          action: "analytics",
+          params: req.params,
+          user: req.user,
+        }),
+      },
+    ],
+  });
+  // const communities = await Community.find({ creatorID: req.user.id }).limit(
+  //   10
+  // );
+  // if (communities.length > 0) {
+  //   let communityIds = [];
+  //   const communityInfo = communities.map((community) => {
+  //     return {
+  //       communityName: community.communityName,
+  //       userCount: community.subscribers.length,
+  //     };
+  //   });
+  //   const postInfo = communities.map((community) => {
+  //     return {
+  //       communityName: community.communityName,
+  //       postCount: community.posts.length,
+  //     };
+  //   });
 
-    let postsInfo = [];
-    for (let i = 0; i < communities.length; i++) {
-      if (communities[i]) {
-        for (let j = 0; j < communities[i].posts.length; j++) {
-          const post = await sqlDB.getUpVotesforPost(communities[i].posts[j]);
-          postsInfo = postsInfo.concat({
-            communityName: communities[i].communityName,
-            postInfo: post,
-          });
-        }
-        communityIds = communityIds.concat(communities[i]._id);
-      }
-    }
+  //   let postsInfo = [];
+  //   for (let i = 0; i < communities.length; i++) {
+  //     if (communities[i]) {
+  //       for (let j = 0; j < communities[i].posts.length; j++) {
+  //         const post = await sqlDB.getUpVotesforPost(communities[i].posts[j]);
+  //         postsInfo = postsInfo.concat({
+  //           communityName: communities[i].communityName,
+  //           postInfo: post,
+  //         });
+  //       }
+  //       communityIds = communityIds.concat(communities[i]._id);
+  //     }
+  //   }
 
-    const userCount = await sqlDB.getUserWithPostCount(communityIds);
-    const maxPost = await userCount.map(async (usr) => {
-      return {
-        user: usr.creatorName,
-        postCount: usr.postCount,
-      };
-    });
-    const userMaxPost = await Promise.all(maxPost);
+  //   const userCount = await sqlDB.getUserWithPostCount(communityIds);
+  //   const maxPost = await userCount.map(async (usr) => {
+  //     return {
+  //       user: usr.creatorName,
+  //       postCount: usr.postCount,
+  //     };
+  //   });
+  //   const userMaxPost = await Promise.all(maxPost);
 
-    postsInfo.sort((a, b) => b.postInfo.upvotes - a.postInfo.upvotes);
-    userMaxPost.sort((c, d) => d.postCount - c.postCount);
-    communityInfo.sort((e, f) => f.userCount - e.userCount);
-    if (postsInfo.length > 10) {
-      postsInfo.length = 10;
-    }
-    if (userMaxPost.length > 10) {
-      userMaxPost.length = 10;
-    }
-    res.json({
-      MaxUserCount: communityInfo,
-      MaxPostCount: postInfo,
-      UpvotedPost: postsInfo,
-      UserMaxPost: userMaxPost,
-    });
-  }
-else{
-    res.send("You do not own a community")
-}
+  //   postsInfo.sort((a, b) => b.postInfo.upvotes - a.postInfo.upvotes);
+  //   userMaxPost.sort((c, d) => d.postCount - c.postCount);
+  //   communityInfo.sort((e, f) => f.userCount - e.userCount);
+  //   if (postsInfo.length > 10) {
+  //     postsInfo.length = 10;
+  //   }
+  //   if (userMaxPost.length > 10) {
+  //     userMaxPost.length = 10;
+  //   }
+  //   res.json({
+  //     MaxUserCount: communityInfo,
+  //     MaxPostCount: postInfo,
+  //     UpvotedPost: postsInfo,
+  //     UserMaxPost: userMaxPost,
+  //   });
+  // } else {
+  //   res.send("You do not own a community");
+  // }
 };
--- server/controllers/communitySearchController.js
+++ server/controllers/communitySearchController.js
@@ -1,5 +1,10 @@
 import { sqlDB } from "../config/queries.js";
 import Community from "../models/Community.js";
+import { communitySearchReqProducer } from "../kafka/producers/communitySearchReqProducer.js";
+import { responses } from "../kafka/kafka.js";
+import { communitySearchResConsumer } from "../kafka/consumers/communitySearchResConsumer.js";
+
+communitySearchReqProducer.connect();
 
 export let communitySearchController = {};
 
@@ -7,34 +12,50 @@ export let communitySearchController = {};
 // @desc search community
 // @access Public
 communitySearchController.searchCommunity = async (req, res) => {
-  const { filter } = req.params;
-  let communities = [];
-  if (!filter) {
-    communities = await Community.find();
-  } else {
-    communities = await Community.find({
-      communityName: { $regex: filter, $options: "i" },
-    });
-  }
-  if (communities) {
-       const upvotesPost = await Promise.all(
-          communities.map(async (community) => { 
-           const count = await sqlDB.sumOfAllUpvotesForPosts(community.id)
-          return {
-          id: community.id,
-          title: community.communityName,
-          description: community.description,
-          image: community.images,
-          postsCount: community.posts.length,
-          date: community.createdDate,
-          users: community.subscribers.length,
-          votes: Math.abs(
-            community.upvotes.length - community.downvotes.length
-          ),
-          postUpvotes: count[0].upvotes,
-          }
-        })
-        );
-        res.send(upvotesPost);
-      }
-    }
+  const requestId = Math.random().toString(36).substr(2);
+  responses[requestId] = res;
+  console.log(requestId);
+  communitySearchReqProducer.send({
+    topic: "commsearch_request",
+    messages: [
+      {
+        value: JSON.stringify({
+          id: requestId,
+          action: "searchCommunity",
+          params: req.params,
+          user: req.user,
+        }),
+      },
+    ],
+  });
+  // const { filter } = req.params;
+  // let communities = [];
+  // if (!filter) {
+  //   communities = await Community.find();
+  // } else {
+  //   communities = await Community.find({
+  //     communityName: { $regex: filter, $options: "i" },
+  //   });
+  // }
+  // if (communities) {
+  //   const upvotesPost = await Promise.all(
+  //     communities.map(async (community) => {
+  //       const count = await sqlDB.sumOfAllUpvotesForPosts(community.id);
+  //       return {
+  //         id: community.id,
+  //         title: community.communityName,
+  //         description: community.description,
+  //         image: community.images,
+  //         postsCount: community.posts.length,
+  //         date: community.createdDate,
+  //         users: community.subscribers.length,
+  //         votes: Math.abs(
+  //           community.upvotes.length - community.downvotes.length
+  //         ),
+  //         postUpvotes: count[0].upvotes,
+  //       };
+  //     })
+  //   );
+  //   res.send(upvotesPost);
+  // }
+};
