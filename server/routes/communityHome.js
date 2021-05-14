5c1bf9041c7a055f55f5ee10d644ec2a806d5f17
Abhijeet Padwal <38448205+padwalab@users.noreply.github.com>
2021-05-14 02:16:07 -0700

    final dev fixes

--- server/routes/communityHome.js
+++ server/routes/communityHome.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import { communityHomeController } from '../controllers/communityHomeController.js';
-import { auth } from '../config/auth.js';
+import express from "express";
+import { communityHomeController } from "../controllers/communityHomeController.js";
+import { auth } from "../config/auth.js";
 
 const router = express.Router();
 export default router;
 
-router.post('/join-community', auth, communityHomeController.requestToJOin);
-router.get('/:communityId', auth, communityHomeController.getCommunityInfo);
-router.delete('/', auth, communityHomeController.leaveCommunity);
+router.post("/join-community", auth, communityHomeController.requestToJOin);
+router.get("/:communityId", communityHomeController.getCommunityInfo);
+router.delete("/", auth, communityHomeController.leaveCommunity);
